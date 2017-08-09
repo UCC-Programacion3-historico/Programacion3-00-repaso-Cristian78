22d9967d33654130bcfe43dcce9960f8593ea47b
Cristian78 <kamadacristian@gmail.com>
2017-08-09 10:30:35 -0300

    Se implemento el ejercicio II (Calculadora)

--- 02_Templates/main.cpp
+++ 02_Templates/main.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
 #include "Calculadora.h"
+#include <string>
 
 using namespace std;
 
 int main() {
+
+    Calculadora<int> calint;
+    Calculadora<char> calchar;
+    Calculadora<float> calfloat;
+    Calculadora<string> calstring;
+
+    // Calculadora de ints
+    calint.setA(4);
+    calint.setB(5);
+
+    cout<<calint.sumar()<<endl;
+    cout<<calint.dividir()<<endl<<endl;
+
+    // Calculadora de chars
+    calchar.setA(4);
+    calchar.setB(5);
+
+    cout<<calchar.sumar()<<endl;
+    cout<<calchar.dividir()<<endl<<endl;
+
+    // Calculadora de float
+    calfloat.setA(4);
+    calfloat.setB(5);
+
+    cout<<calfloat.sumar()<<endl;
+    cout<<calfloat.dividir()<<endl<<endl;
+
+    // Calculadora de string
+    calstring.setA("Hola ");
+    calstring.setB("mundo");
+
+    cout<<calstring.sumar()<<endl;
+
     cout << "Ejercicio 00/02\n" << endl;
     return 0;
 }
